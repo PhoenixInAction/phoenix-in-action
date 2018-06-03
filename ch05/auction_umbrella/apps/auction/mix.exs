dfb220ade46e0b84eb66ffe81a05e0e8009cefb8
Geoffrey Lessel <geo@geoffreylessel.com>
2018-06-03 13:58:28 -0700

    Add ecto and postgrex to our deps

--- ch05/auction_umbrella/apps/auction/mix.exs
+++ ch05/auction_umbrella/apps/auction/mix.exs
@@ -26,9 +26,8 @@ defmodule Auction.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
+      {:ecto, "~> 2.2.7"},
+      {:postgrex, "~> 0.13.3"}
     ]
   end
 end
