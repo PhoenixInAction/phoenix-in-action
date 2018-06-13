a76e2195568cb4614cf5fb5c40b9a69a46a0a2b2
Geoffrey Lessel <geo@geoffreylessel.com>
2018-06-12 21:13:12 -0700

    Add initial ch07 code from ch06 end

---
+++ ch07/auction_umbrella/apps/auction/lib/auction.ex
@@ -0,0 +1,17 @@
+defmodule Auction do
+  alias Auction.{FakeRepo, Item}
+
+  @repo FakeRepo
+
+  def list_items do
+    @repo.all(Item)
+  end
+
+  def get_item(id) do
+    @repo.get!(Item, id)
+  end
+
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
+  end
+end
---
+++ ch07/auction_umbrella/apps/auction_web/lib/auction_web/application.ex
@@ -0,0 +1,27 @@
+defmodule AuctionWeb.Application do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the endpoint when the application starts
+      supervisor(AuctionWeb.Endpoint, []),
+      # Start your own worker by calling: AuctionWeb.Worker.start_link(arg1, arg2, arg3)
+      # worker(AuctionWeb.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: AuctionWeb.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    AuctionWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
