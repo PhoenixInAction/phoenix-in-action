4332d832e25d2154e16655ec328567478a624d4a
Geoffrey Lessel <geo@geoffreylessel.com>
2018-06-18 14:58:14 -0700

    Add chapter 8 code

---
+++ ch08/auction_umbrella/apps/auction/lib/auction.ex
@@ -0,0 +1,31 @@
+defmodule Auction do
+  alias Auction.{Item}
+
+  @repo Auction.Repo
+
+  def list_items do
+    @repo.all(Item)
+  end
+
+  def get_item(id) do
+    @repo.get!(Item, id)
+  end
+
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
+  end
+
+  def insert_item(attrs) do
+    Item
+    |> struct(attrs)
+    |> @repo.insert()
+  end
+
+  def delete_item(%Item{} = item), do: @repo.delete(item)
+
+  def update_item(%Item{} = item, updates) do
+    item
+    |> Item.changeset(updates)
+    |> @repo.update()
+  end
+end
